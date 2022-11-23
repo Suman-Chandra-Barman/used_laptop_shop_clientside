8b47ce4c4c966a0901e72b8146b4a819b1a58a9e
Suman-Chandra-Barman <sumandranathbarman@gmail.com>
2022-11-24 00:10:18 +0600

    Navbar and Footer page created

---
+++ src/routes/Router.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { createBrowserRouter } from "react-router-dom";
+import Main from "../layouts/Main";
+import Home from "../pages/Home/Home";
+import Login from "../pages/Login/Login";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Main />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/login",
+        element: <Login />,
+      },
+    ],
+  },
+]);
+
+export default router;
